98def732303a3f365d04f758efd491d81a0806e1
olahsymbo <olahsymbo@gmail.com>
2021-03-30 11:07:24 +0100

    Added js and html

--- static/index.js
+++ static/index.js
@@ -1,21 +1,25 @@
 // Form reference
 const form = {}
+form.noteTitle = document.querySelector('#titleNoteText');
 form.noteText = document.querySelector('#formNoteText');
 form.addButton = document.querySelector('#formAddButton');
 form.color = document.querySelector('#formColor');
 
 const notes = document.querySelector('#notes');
 
+form.noteTitle.focus();
 form.noteText.focus();
 
 // Functions
 function addNote() {
+  let title = form.noteTitle.value;
   let text = form.noteText.value;
   let note = document.createElement('div');
   let deleteButton = document.createElement('span');
 
   note.classList.add('note');
   note.classList.add(form.color.value);
+  note.innerHTML = `<div class='note-title'>${title}</div>`;
   note.innerHTML = `<div class='note-text'>${text}</div>`;
   deleteButton.classList.add('note-delete');
   deleteButton.innerHTML = '&times;';
@@ -23,7 +27,9 @@ function addNote() {
   note.appendChild(deleteButton);
   notes.appendChild(note);
 
+  form.noteTitle.value = '';
   form.noteText.value = '';
+  form.noteTitle.focus();
   form.noteText.focus();
 
   addListenerDeleteButton(deleteButton);
@@ -41,12 +47,10 @@ function deleteNote(e) {
   eventNote.parentNode.removeChild(eventNote);
 }
 
-
-
 // Event Listeners
 form.addButton.addEventListener('click', function (e) {
   e.preventDefault();
-  if (form.noteText.value != '') {
+  if ((form.noteTitle.value != '') && (form.noteText.value != '')) {
     addNote();
   }
 })
