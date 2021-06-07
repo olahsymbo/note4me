7bb1f4bfa0855a7066119bd4cbac433444c398c6
Olasimbo <olahsymbo@gmail.com>
2021-06-07 13:41:26 +0100

    added precommit hooks

--- NoteApp/tests.py
+++ NoteApp/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Notes, Category
+
+
+class NoteTest(TestCase):
+    def setUp(self):
+        cate = Category.objects.create(name="general")
+        self.notes = Notes.objects.create(
+            title="Technology",
+            content="New tech piece",
+            created=timezone.now().strftime("%Y-%m-%d"),
+            due_date=timezone.now().strftime("%Y-%m-%d"),
+            category=cate,
+        )
+
+    def test_get_note(self):
+        new_note = self.notes
+        self.assertTrue(isinstance(new_note, Notes))
